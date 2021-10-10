2a795cddcdb5393e1c2732bddc17eaed2ca5aca8
João Bosco <joaobosconff03@gmail.com>
2021-10-09 23:11:55 -0300

    updated

--- src/main/java/tech/jhipster/registry/config/JacksonConfiguration.java
+++ src/main/java/tech/jhipster/registry/config/JacksonConfiguration.java
@@ -2,10 +2,9 @@ package tech.jhipster.registry.config;
 
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.zalando.problem.ProblemModule;
+import org.zalando.problem.jackson.ProblemModule;
 import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
 @Configuration
@@ -23,14 +22,6 @@ public class JacksonConfiguration {
     @Bean
     public Jdk8Module jdk8TimeModule() {
         return new Jdk8Module();
-    }
-
-    /*
-     * Jackson Afterburner module to speed up serialization/deserialization.
-     */
-    @Bean
-    public AfterburnerModule afterburnerModule() {
-        return new AfterburnerModule();
     }
 
     /*
